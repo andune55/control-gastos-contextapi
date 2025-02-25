de1ec3695e85a682a3884dbc5aa7caa7acb80025
Agustin Aguilera Beltran <pw-aaguilera@KM-5CD30206YX.atsistemas.lan>
2025-02-25 18:17:48 +0100

    avance curso 210

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,11 +1,60 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
 
 export default function ExpenseForm() {
+
+  const [expense, setExpense] = useState<DraftExpense>({
+    amount: 0,
+    expenseName: '',
+    category: '',
+    date: new Date()
+  })
+
+  const [error, setError] = useState('')
+  const { dispatch } = useBudget()
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target //extraemos name y value de cada campo de formulario
+    const isAmountField = ['amount'].includes(name) //true si estamos escribiendo en amount y false si escribimos en gasto o seleccionamos categoría
+    //console.log(isAmountField)
+    setExpense({
+      ...expense,
+      [name] : isAmountField ? +value : value
+    })
+  }
+
+  const handleChangeDate = (value : Value) =>{
+    //console.log(value)
+    setExpense({
+      ...expense,
+      date: value
+    })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    //validar
+    if(Object.values(expense).includes('')){
+      setError('Todos los campos son obligatorios')
+    }
+    //Agregar un nuevo gasto
+    dispatch({type: 'add-expense', payload: {expense}})
+  }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend
         className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
       >Nuevo gasto</legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <div className="flex flex-col gap-2">
         <label 
@@ -18,7 +67,10 @@ export default function ExpenseForm() {
           id="expenseName"
           placeholder="Añade el nombre del gasto"
           className="bg-slate-100 p-2"
-          name="expenseName" />
+          name="expenseName"
+          value={expense.expenseName} 
+          onChange={handleChange}
+        />
       </div>
 
       <div className="flex flex-col gap-2">
@@ -32,7 +84,10 @@ export default function ExpenseForm() {
           id="amount"
           placeholder="Añade la cantidad del gasto: ej. 300"
           className="bg-slate-100 p-2"
-          name="amount" />
+          name="amount"
+          value={expense.amount} 
+          onChange={handleChange}
+          />
       </div>
 
       <div className="flex flex-col gap-2">
@@ -44,21 +99,38 @@ export default function ExpenseForm() {
         <select 
           id="category"
            className="bg-slate-100 p-2"
-          name="category" >
+          name="category" 
+          value={expense.category}
+          onChange={handleChange}
+          >
             <option value="">-- Selecciona --</option>
             {categories.map(category => (
               <option 
                 key={category.id}
-                value="categorie.id"
+                value={category.id}
                 >{category.name}</option>
             ))}
           </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label 
+          htmlFor="amount"
+          className="text-xl">
+          Fecha gasto:
+        </label>
+        <DatePicker
+          className="bg-slate-100 p-2 border-0"
+          value={expense.date}
+          onChange={handleChangeDate}
+        />
       </div>
       
             <input 
               type="submit" 
               className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
-              value={'Registrar gasto'}
+              //value={'Registrar gasto'}
+              onChange={()=>{}}
             />
 
     </form>

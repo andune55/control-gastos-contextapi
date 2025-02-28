4a011a639fd34a064f9c44da424eab50176cf266
Agustin <latierramedia@gmail.com>
2025-02-28 16:26:40 +0100

    curso 225

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,20 +1,38 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import { useBudget } from '../hooks/useBudget';
 import AmountDisplay from './AmountDisplay';
+import "react-circular-progressbar/dist/styles.css"
 
 export default function BudgetTracker() {
-    const { state, remainingBudget, totalExpenses } = useBudget()
+    const { state, remainingBudget, totalExpenses, dispatch } = useBudget()
+
+    const percentage = +((totalExpenses / state.budget) * 100).toFixed(2)
+    //console.log(percentage)
+
+    const resetApp = () =>{dispatch({type: 'restart-app'}) }
 
     return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <div className="flex justify-center">
-            <img src="./grafico.jpg" alt="Gráfica de gastos" />
+            <CircularProgressbar 
+                value={percentage}
+                styles={buildStyles({
+                    pathColor: percentage >= 80? '#dc2626' : '#3b82f6',
+                    trailColor: '#f5f5f5',
+                    textSize: 8,
+                    textColor: percentage >= 80? '#dc2626' : '#3b82f6'
+                })}
+                text={`${percentage}% gastado`}                
+            />
         </div>
 
         <div className="flex flex-col justify-center items-center gap-8">
             <button
                 type="button"
-                className='bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg'>
-                    Resetear App
+                className='bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg cursor-pointer'
+                onClick={resetApp}
+            >
+                Resetear App
             </button>
 
             <AmountDisplay
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -8,7 +8,8 @@ export type BudgetActions =
     { type: 'add-expense', payload: {expense: DraftExpense} } |
     { type: 'remove-expense', payload: {id: Expense['id']} } |
     { type: 'get-expense-by-id', payload: {id: Expense['id']} } |
-    { type: 'update-expense', payload: {expense: Expense} } 
+    { type: 'update-expense', payload: {expense: Expense} } | 
+    { type: 'restart-app' } 
 
 export type BudgetState = {
     budget: number
@@ -90,6 +91,13 @@ export const budgetReducer = (
             expenses: state.expenses.map(expense => expense.id === action.payload.expense.id ? action.payload.expense : expense),
             modal: false,
             editingId: ''
+        }
+    }
+    if(action.type == "restart-app" ){
+        return{
+            ...state,
+            budget: 0,
+            expenses: []
         }
     }
 

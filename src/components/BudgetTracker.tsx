a1512c6a84d0ee2a25e2ec1fb26ce9d571d937cc
aab <latierramedia@gmail.com>
2025-03-05 16:16:06 +0100

    mejor versión rediseño1 05/03

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -4,13 +4,11 @@ import AmountDisplay from './AmountDisplay';
 import "react-circular-progressbar/dist/styles.css"
 
 export default function BudgetTracker() {
-    const { state, remainingBudget, totalExpenses, dispatch } = useBudget()
+    const { state, remainingBudget, totalExpenses } = useBudget()
 
     const percentage = +((totalExpenses / state.budget) * 100).toFixed(2)
     //console.log(percentage)
-
-    const resetApp = () =>{dispatch({type: 'restart-app'}) }
-
+    
     return (
     <div className="flex max-sm:flex-col gap-5">
         <div className="flex justify-center max-h-[200px] max-sm:max-h-[150px]">
@@ -27,14 +25,6 @@ export default function BudgetTracker() {
         </div>
 
         <div className="flex flex-col justify-items-start items-center gap-2">
-            {/* <button
-                type="button"
-                className='bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg cursor-pointer'
-                onClick={resetApp}
-            >
-                Resetear App
-            </button> */}
-
             <AmountDisplay
                 label="Presupuesto"
                 amount={state.budget}
